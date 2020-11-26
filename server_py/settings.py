89f6a8ea5100c5e0f345b0c18fb0bbac0ef4ef92
Tan Wai Liang <tanwailiang97@gmail.com>
2020-11-27 02:11:10 +0800

    fix syntax

--- server_py/settings.py
+++ server_py/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [a17kedjango.herokuapp.com]
+ALLOWED_HOSTS = ["a17kedjango.herokuapp.com"]
 
 
 # Application definition
