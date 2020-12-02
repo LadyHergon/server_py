a8c828b303a80048c62f8a1a18862284e5bf70e5
Tan Wai Liang <tanwailiang97@gmail.com>
2020-12-02 10:53:57 +0800

    image alt
    Image path changed
    
    google drive image
    
    added fake spectogram
    
    fix image file path
    
    wrong commit
    
    Update README.md

--- server_py/settings.py
+++ server_py/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
+DEBUG = True#(os.environ.get('DEBUG_VALUE') == 'TRUE')
 
-ALLOWED_HOSTS = ["a17kedjango.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost:8000","a17kedjango.herokuapp.com"]
 
 
 # Application definition
