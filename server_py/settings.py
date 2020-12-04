9d8189b48a4ac25ceecbaebe110b39f19aa3ae9f
Tan Wai Liang <tanwailiang97@gmail.com>
2020-12-05 05:19:41 +0800

    gd storage 1st try

--- server_py/settings.py
+++ server_py/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'gdstorage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -144,5 +145,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER =  os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = None
 
 django_heroku.settings(locals())
--- users/models.py
+++ users/models.py
@@ -1,30 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from gdstorage.storage import GoogleDriveStorage
+
+gd_storage = GoogleDriveStorage()
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to ='profile_pics')
+    image = models.ImageField(default='default.png', upload_to ='profile_pics', storage=gd_storage)
 
     def __str__(self):
         return f'{self.user.username} Profile' 
 
-    def save(self, *args, **kwargs):
+    # def save(self, *args, **kwargs):
 
-        try:
-            this = Profile.objects.get(id=self.id)
-            if this.image != self.image:
-                this.image.delete(save=False)
-        except: pass
+    #     try:
+    #         this = Profile.objects.get(id=self.id)
+    #         if this.image != self.image:
+    #             this.image.delete(save=False)
+    #     except: pass
 
-        super(Profile,self).save(*args, **kwargs)
+    #     super(Profile,self).save(*args, **kwargs)
         
-        img =Image.open(self.image.path)
+    #     img =Image.open(self.image.path)
 
-        if img.height > 300 or img.width >300:
-            output_size = (300,300)
-            img.thumbnail(output_size,Image.ANTIALIAS)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width >300:
+    #         output_size = (300,300)
+    #         img.thumbnail(output_size,Image.ANTIALIAS)
+    #         img.save(self.image.path)
 
 
