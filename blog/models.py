703c6db87181784aaec6366e035cfba2ec945705
Tan Wai Liang <tanwailiang97@gmail.com>
2020-12-30 03:31:40 +0800

    rebase 3

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-from gdstorage.storage import GoogleDriveStorage
+from gdstorage.storage import GoogleDriveStorage, GoogleDrivePermissionType, GoogleDrivePermissionRole, GoogleDriveFilePermission
 
 # from django.core.files.storage import default_storage as storage
 
