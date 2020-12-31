5860123315d0fbbea3d4fd6b23286596d49ce1fc
Tan Wai Liang <tanwailiang97@gmail.com>
2020-12-31 09:30:45 +0800

    Merging

--- blog/models.py
+++ blog/models.py
@@ -7,30 +7,15 @@ from gdstorage.storage import GoogleDriveStorage
 
 gd_storage = GoogleDriveStorage()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 from .sound import Sound
 import uuid
 
-=======
->>>>>>> 5905079 (Moved logic to view)
-=======
->>>>>>> c31c90e11e1db9a4bb82a880b2d1b152839d4c13
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     audio = models.FileField(null=True, upload_to='musics')
-<<<<<<< HEAD
-<<<<<<< HEAD
-    image = models.ImageField(default='audio-image/' + str(uuid.uuid4()) + ".png" ,
-                            null=True, upload_to='audio-image', storage=gd_storage)
-=======
-    image = models.ImageField(default="abc.png",null=True, upload_to='audio-image', storage=gd_storage)
->>>>>>> 5905079 (Moved logic to view)
-=======
-    image = models.ImageField(default="abc.png",null=True, upload_to='audio-image', storage=gd_storage)
->>>>>>> c31c90e11e1db9a4bb82a880b2d1b152839d4c13
+    image = models.ImageField(null=True, upload_to='audio-image', storage=gd_storage)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
 
     def __str__(self):
@@ -38,20 +23,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-    def save(self,*args, **kwargs):
-        super().save()
 
-        audioFile = Sound(self.audio.path)
-        audioFile.getImage(self.image.path)
-=======
-        
->>>>>>> 5905079 (Moved logic to view)
-=======
-        
->>>>>>> c31c90e11e1db9a4bb82a880b2d1b152839d4c13
+    # def save(self,*args, **kwargs):
+    #     super().save()
+
+    #     audioFile = Sound(self.audio.path)
+    #     audioFile.getImage(self.image.path)
 
 
 
