166a5bb645088a56c1da2717c9dd3d7064043389
Nur Hayati Sahrir <73183739+LadyHergon@users.noreply.github.com>
2021-01-02 00:00:07 +0800

    Update sound.py
    + duration, file size, change file type to wav, sampling freq

--- blog/sound.py
+++ blog/sound.py
@@ -21,3 +21,20 @@ def Sound(audioFile):
     plt.savefig(figure, bbox_inches='tight',format="png")
     plt.close()
     return InMemoryUploadedFile(figure,'ImageField','image','image/jpeg',sys.getsizeof(figure),None)
+def Duration(audioFile):
+    Duration = lr.get_duration(y=None, sr=22050, S=None, n_fft=2048, hop_length=512, center=True, filename=audioFile)
+    return Duration
+
+def Fsize(audioFile):
+    Fsize = lr.get_samplerate(audioFile)
+    return Fsize
+
+def Ftype(audioFile):
+    rate = 44100
+    data = np.random.uniform(-1, 1, size=(rate * 10, 2))
+    Ftype = sf.write(audioFile, data, lr.get_samplerate(audioFile), subtype='PCM_24')
+    return Ftype
+
+def SampFreq(audioFile):
+    audio, sfreq = lr.load(audioFile)
+    return sfreq
